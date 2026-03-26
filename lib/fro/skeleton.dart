c2ded16334bfa8ec7d7aead0f7f0c22f41e8ab1a
chenzhigang <979703986@qq.com>
2026-03-26 14:56:23 +0800

    chore: 初始化项目结构

---
+++ lib/fro/skeleton.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FroSkeleton extends StatefulWidget {
+  const FroSkeleton({super.key});
+
+  @override
+  State<FroSkeleton> createState() => _FroSkeletonState();
+}
+
+class _FroSkeletonState extends State<FroSkeleton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
