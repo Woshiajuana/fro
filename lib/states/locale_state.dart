e4ec283939ea19f2708953efec969ef0f5098769
chenzhigang <979703986@qq.com>
2026-04-01 17:28:17 +0800

    chore: 提交

--- lib/states/locale_state.dart
+++ lib/states/locale_state.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:fro/fro/utils/index.dart';
 
 class LocaleState extends ChangeNotifier {
-  // 语言 code
-  late String _languageCode;
+  LocaleState()
+    : _languageCode = _languageCodeStorage.getItem(_systemLanguageCode);
+
+  static const String _systemLanguageCode = 'system';
+  static const List<String> _supportedCodes = ['en', 'zh'];
+  static final FroStorage<String> _languageCodeStorage = FroStorage<String>(
+    key: 'app.locale.languageCode',
+    initialValue: _systemLanguageCode,
+  );
+
+  String _languageCode;
+
   String get languageCode => _languageCode;
 
-  //  语言
   Locale? get locale {
-    // 跟随系统
-    if (_languageCode != 'system') {
-      return Locale(_languageCode);
+    if (_languageCode == _systemLanguageCode) {
+      return null;
     }
-    return null;
+    return Locale(_languageCode);
+  }
+
+  bool get isFollowSystem => _languageCode == _systemLanguageCode;
+
+  Future<void> setLanguageCode(String languageCode) async {
+    final String nextCode = _normalizeCode(languageCode);
+    if (_languageCode == nextCode) {
+      return;
+    }
+    _languageCode = nextCode;
+    notifyListeners();
+    await _languageCodeStorage.setItem(_languageCode);
+  }
+
+  Future<void> followSystem() => setLanguageCode(_systemLanguageCode);
+
+  Future<void> useEnglish() => setLanguageCode('en');
+
+  Future<void> useChinese() => setLanguageCode('zh');
+
+  String _normalizeCode(String languageCode) {
+    if (languageCode == _systemLanguageCode) {
+      return _systemLanguageCode;
+    }
+    return _supportedCodes.contains(languageCode) ? languageCode : 'zh';
   }
 }
--- lib/states/theme_state.dart
+++ lib/states/theme_state.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:fro/fro/utils/index.dart';
 
 class ThemeState extends ChangeNotifier {
-  /// 主题模式
-  late String _themeModeName;
+  ThemeState()
+    : _themeModeName = _themeModeStorage.getItem(ThemeMode.system.name);
+
+  static final FroStorage<String> _themeModeStorage = FroStorage<String>(
+    key: 'app.theme.mode',
+    initialValue: ThemeMode.system.name,
+  );
+
+  String _themeModeName;
+
   String get themeModeName => _themeModeName;
 
-  // 主题模式
   ThemeMode get themeMode {
-    if (_themeModeName == 'light') {
-      return ThemeMode.light;
-    } else if (_themeModeName == 'dark') {
-      return ThemeMode.dark;
-    } else {
-      return ThemeMode.system;
+    switch (_themeModeName) {
+      case 'light':
+        return ThemeMode.light;
+      case 'dark':
+        return ThemeMode.dark;
+      default:
+        return ThemeMode.system;
     }
   }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    if (_themeModeName == mode.name) {
+      return;
+    }
+    _themeModeName = mode.name;
+    notifyListeners();
+    await _themeModeStorage.setItem(_themeModeName);
+  }
+
+  Future<void> useSystem() => setThemeMode(ThemeMode.system);
+
+  Future<void> useLight() => setThemeMode(ThemeMode.light);
+
+  Future<void> useDark() => setThemeMode(ThemeMode.dark);
 }
