928f1af84569b5b8fff1ad1bb710c2b9bc2e21f3
chenzhigang <979703986@qq.com>
2026-04-01 17:47:39 +0800

    chore: 更新页面

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,47 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fro/fro/utils/index.dart';
-
-class AppThemeProvider extends ChangeNotifier {
-  AppThemeProvider()
-    : _themeMode = _parseThemeMode(
-        _themeModeStorage.getItem(ThemeMode.system.name),
-      );
-
-  static final FroStorage<String> _themeModeStorage = FroStorage<String>(
-    key: 'theme.mode',
-    initialValue: ThemeMode.system.name,
-  );
-
-  ThemeMode _themeMode;
-
-  ThemeMode get themeMode => _themeMode;
-
-  Future<void> setThemeMode(ThemeMode mode) async {
-    if (_themeMode == mode) {
-      return;
-    }
-    _themeMode = mode;
-    notifyListeners();
-    await _themeModeStorage.setItem(mode.name);
-  }
-
-  Future<void> useSystem() => setThemeMode(ThemeMode.system);
-
-  Future<void> useLight() => setThemeMode(ThemeMode.light);
-
-  Future<void> useDark() => setThemeMode(ThemeMode.dark);
-
-  static ThemeMode _parseThemeMode(String value) {
-    switch (value) {
-      case 'light':
-        return ThemeMode.light;
-      case 'dark':
-        return ThemeMode.dark;
-      default:
-        return ThemeMode.system;
-    }
-  }
-}
 
 class AppTheme {
   AppTheme._();
