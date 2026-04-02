7a38bd1fb4f693a9882cf3a39bf2e3ec7b8d3318
chenzhigang <979703986@qq.com>
2026-04-02 11:32:14 +0800

    chore: 更新

--- lib/fro/widgets/cell/cell.dart
+++ lib/fro/widgets/cell/cell.dart
@@ -4,50 +4,55 @@ import 'package:fro/fro/widgets/clickable/clickable.dart';
 class FroCell extends StatelessWidget {
   const FroCell({
     super.key,
-    this.onTap,
+    this.leading,
+    this.leadingIcon,
     this.labelText,
     this.label,
+    this.descriptionText,
+    this.description,
     this.valueText,
     this.value,
-    this.arrow = false,
-    this.icon,
-    this.leading,
     this.trailing,
+    this.arrow = false,
     this.color,
+    this.onTap,
   });
 
-  /// 点击
-  final void Function()? onTap;
+  /// 左侧组件
+  final Widget? leading;
 
-  /// label text
+  /// 左侧 icon
+  final IconData? leadingIcon;
+
+  /// 标签
   final String? labelText;
 
-  /// label text
+  /// 标签组件
   final Widget? label;
 
-  /// 描述信息
-  final String? description;
+  /// 描述
+  final String? descriptionText;
 
-  /// value text
+  /// 描述组件
+  final Widget? description;
+
+  /// 右侧文案
   final String? valueText;
 
-  /// value widget
+  /// 右侧组件
   final Widget? value;
-
-  /// 右箭头
-  final bool arrow;
-
-  /// leading
-  final Widget? leading;
 
   /// trailing
   final Widget? trailing;
 
-  /// icon
-  final IconData? icon;
+  /// 右箭头
+  final bool arrow;
 
   /// color
   final Color? color;
+
+  /// 点击
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
