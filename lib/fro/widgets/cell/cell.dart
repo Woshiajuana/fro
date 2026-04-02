c68d481d51204d4419ca5376e106bd249f654e19
chenzhigang <979703986@qq.com>
2026-04-02 10:28:20 +0800

    chore: 更新

--- lib/fro/widgets/cell/cell.dart
+++ lib/fro/widgets/cell/cell.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
 
 class FroCell extends StatelessWidget {
-  const FroCell({super.key});
+  const FroCell({
+    super.key,
+    this.onTap,
+    this.labelText,
+    this.label,
+    this.valueText,
+    this.value,
+    this.arrow = false,
+    this.icon,
+    this.leading,
+    this.trailing,
+    this.color,
+  });
+
+  /// 点击
+  final void Function()? onTap;
+
+  /// label text
+  final String? labelText;
+
+  /// label text
+  final Widget? label;
+
+  /// value text
+  final String? valueText;
+
+  /// value widget
+  final Widget? value;
+
+  /// 右箭头
+  final bool arrow;
+
+  /// leading
+  final Widget? leading;
+
+  /// trailing
+  final Widget? trailing;
+
+  /// icon
+  final IconData? icon;
+
+  /// color
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -15,7 +15,7 @@ class AppRouter {
 
   static GoRouter createRouter({List<NavigatorObserver> observers = const []}) {
     return GoRouter(
-      initialLocation: '/language',
+      initialLocation: '/cell',
       observers: observers,
       routes: [
         GoRoute(path: '/', redirect: (context, state) => '/playground'),
