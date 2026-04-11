9c5015a894635d315fd723828f0590690d1dfd4b
chenzhigang <979703986@qq.com>
2026-04-11 22:23:17 +0800

    chore: Gengx

--- lib/pages/setting/setting_page.dart
+++ lib/pages/setting/setting_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fro/extensions/index.dart';
 import 'package:fro/fro/widgets/index.dart';
+import 'package:fro/states/index.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -7,12 +11,40 @@ class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('设置')),
+      appBar: AppBar(title: Text(context.l10n.settingsTitle)),
       body: FroScrollView(
         children: [
           FroCellGroup(
+            titleText: context.l10n.settingsDisplay,
             children: [
-              FroCell(onTap: () => {}, labelText: '下拉菜单弹窗', arrow: true),
+              Consumer<LocaleState>(
+                builder: (context, localeState, _) {
+                  var languageCodeName = localeState.getLanguageCodeName(
+                    context,
+                  );
+                  return FroCell(
+                    onTap: () {
+                      context.push('/setting/language');
+                    },
+                    labelText: context.l10n.settingsLanguage,
+                    value: languageCodeName,
+                    arrow: true,
+                  );
+                },
+              ),
+              Consumer<ThemeState>(
+                builder: (context, themeState, _) {
+                  var themeModeName = themeState.getThemeModeName(context);
+                  return FroCell(
+                    onTap: () {
+                      context.push('/setting/theme');
+                    },
+                    labelText: context.l10n.settingsTheme,
+                    value: themeModeName,
+                    arrow: true,
+                  );
+                },
+              ),
             ],
           ),
         ],
